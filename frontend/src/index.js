6afddaabe4dccc1e41d72233e2f1d2d73346272b
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-02 11:13:13 +0000

    feat: Added store and persistGate to the app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import {appStore, persistedStore} from './store/store';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+<Provider store={appStore}>
+  <PersistGate loading={null} persistor={persistedStore}>
+    <App />
+  </PersistGate>
+</Provider>,
+ document.getElementById('root'));
 registerServiceWorker();
