1970efa89d76d4df0fec9492aeaa6f90bb598d70
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-04 13:53:40 +0000

    feat: Created the redux store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,7 @@
 import { createLogger } from 'redux-logger';
-import appReducer from '../mainRedux/mainReducer';
+import appReducer from '../redux/mainReducer';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
