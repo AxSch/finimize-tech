1f3ddc05e71e6230c4bb4f6d0b4faac0c0329b01
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-06 12:16:14 +0000

    test: Added basic test for utils calculate

---
+++ tests/test_utils.py
@@ -0,0 +1,11 @@
+from interest_calculator.utils import calculate
+
+def test_calculate():
+  savings_amount = 5000
+  monthly_amount = 100
+  interest_rate = 5
+  interest_freq = 12
+  results, result = calculate(savings_amount, monthly_amount, interest_rate, interest_freq)
+  result = round(result, 2)
+  assert(result == 327462.11)
+  assert(len(results) == 50)
