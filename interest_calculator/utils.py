660d6dc3b1b36acbd0926b820046c82bdd530c09
K.A. Schuneman <alex_schuneman@outlook.com>
2018-11-06 12:05:32 +0000

    refact&feat: Added FutureValue fn & refactored calculate

--- interest_calculator/utils.py
+++ interest_calculator/utils.py
@@ -2,11 +2,26 @@
 def calculate(savings_amount, monthly_amount, interest_rate, interest_freq):
   results=[]
   result = 0 
-  t = 50
-  for year in range(t):
-      P = savings_amount + monthly_amount
+  t = 10
+  for year in range(1, t + 1):
+      P = savings_amount
       r = interest_rate/100
       n = interest_freq
-      result = P * (1 + (r/n))**year
+      FV = calculateFutureValue(monthly_amount, interest_rate, interest_freq, year)
+      compound_interest = P * (1 + r/n)**(n * year)
+      result = compound_interest + FV
+      print(result)
       results.append({"year": year, "amount": result})
   return results, result
+
+def calculateFutureValue(monthly_amount, interest_rate, interest_freq, year):
+  PMT = monthly_amount
+  r = interest_rate/100
+  n = interest_freq
+  step0 = 1 + (r/n)
+  step2 = step0**(n * year)
+  step3 = step2 - 1
+  step4 = r / n
+  step5 = step3 / step4
+  result = PMT * step5
+  return result
